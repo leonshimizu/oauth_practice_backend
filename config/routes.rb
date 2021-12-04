a160b92f473d648e90e66f99ce185aa54cb51bee
“leonshimizu” <lmshimizu@gmail.com>
2021-12-04 12:09:54 -0800

    oath mostly complete - everything should be there but not working properly yet - when pulled, make sure to configure credentials file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "news" => "pages#index"
+
+  get "/auth/github/callback" => "github#authorize"
+  get "/github_profile" => "github#profile"
 end
